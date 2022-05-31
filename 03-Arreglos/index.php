baa19b9f8231ba547996bb2ff7ba88bd29d19186
JA Rojas Paredes <paredesjosue77@gmail.com>
2022-05-31 15:00:49 -0500

    Arrgelos

---
+++ 03-Arreglos/index.php
@@ -0,0 +1,28 @@
+
+
+<?php
+
+$array1 = array(
+	"foo" => "bar",
+	"bar" => "foo",
+);
+
+// * Otra manera de hacer
+
+$array2 = [
+	"foo" => "bar",
+	"bar" => "foo"
+];
+
+
+$array3 = array('Josue','Marin','Abigail','Cielo');
+
+
+
+echo array1;
+echo array2;
+
+echo array3[1];
+
+
+?>
